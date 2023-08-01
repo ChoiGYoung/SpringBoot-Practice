5b67c52e6b41b25b2086d8a69f0308af9f81fd3c
ChoiGYoung <choidh1261@naver.com>
2023-08-01 14:49:46 +0900

    [study] : 컴포넌트 스캔과 자동 의존관계 설정

--- src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -37,7 +37,7 @@ class MemberServiceTest {
     }
 
     @Test
-    public void 중복회원예외(){
+    public void 중복_회원_예외() throws Exception {
         //given
         Member member1 = new Member();
         member1.setName("spring");
@@ -45,11 +45,11 @@ class MemberServiceTest {
         Member member2 = new Member();
         member2.setName("spring");
 
-        //when
-        memberService.join(member1);
-        IllegalStateException e = assertThrows(IllegalStateException.class,
-                () -> memberService.join(member2));//예외가 발생해야 한다.
-        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+        //When
+//        memberService.join(member1);
+//        IllegalStateException e = assertThrows(IllegalStateException.class,
+//                () -> memberService.join(member2));//예외가 발생해야 한다.
+//        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
 
 
 //        memberService.join(member1);
@@ -63,11 +63,4 @@ class MemberServiceTest {
         //then
     }
 
-    @Test
-    void findMembers() {
-    }
-
-    @Test
-    void findOne() {
-    }
 }
